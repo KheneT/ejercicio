41c89fe9460d645debea8e8c9aff16a78cc14c82
Juan Daniel Gonzalez <juandanielgonzalez004@gmail.com>
2023-03-04 22:19:11 -0500

    Creación constructor y atributos

--- Dispositivos.cs
+++ Dispositivos.cs
@@ -1,8 +1,22 @@
 ﻿using System;
 
-public class Class1
+public class Dispositivos
 {
-	public Class1()
+	public Dispositivos(nombre,descripción,precio,cantidadDisponible,fecha)
 	{
+		this.nombre = nombre;
+		this.precio = precio;
+		this.descripción = descripción;
+		this.cantidadDisponible = cantidadDisponible;
+		this.fecha = fecha;
 	}
+
+
+
+	private string nombre,descripción;
+	private double precio;
+	private int cantidadDisponible;
+	private dateTime fecha = new dateTime();
+
+	
 }
