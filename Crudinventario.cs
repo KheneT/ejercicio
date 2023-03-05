3d85ee8ba870abd29398516dab0655d462d9cceb
Navisper <alobuenastarde@gmail.com>
2023-03-04 22:48:56 -0500

    clase abstracta crudinventario

---
+++ Crudinventario.cs
@@ -0,0 +1,35 @@
+using System;
+
+namespace dispositivo
+{
+    public abstract class CrudInventario
+    {   
+        protected List<Dispositivo> productos = new List<Dispositivo>();
+
+        public virtual void AgregarProducto(Dispositivo producto)
+        {
+            Dispositivo.Add(producto);
+        }
+
+        public virtual void ActualizarProducto(Producto producto)
+        {
+            var index = Dispositivo.FindIndex(p => p.Nombre == producto.Nombre);
+            if (index != -1)
+            {
+                Dispositivo[index] = producto;
+            }
+        }
+
+        public virtual void EliminarProducto(Dispositivo producto)
+        {
+            productos.Remove(producto);
+        }
+
+        public abstract List<Dispositivo> BuscarPorNombre(string nombre);
+
+        public abstract List<Dispositivo> BuscarPorPrecio(double precioMinimo, double precioMaximo);
+
+        
+    }   
+ 
+}
